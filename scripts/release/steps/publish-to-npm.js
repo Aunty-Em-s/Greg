aaf682c224212bf77a82e999d25a266723cb26ab
Michael Ellison <167623127+AuntyEms@users.noreply.github.com>
2024-09-15 00:01:46 +1000

    Initialize dependabot/npm_and_yarn/postcss-8.4.31

---
+++ scripts/release/steps/publish-to-npm.js
@@ -0,0 +1,44 @@
+import { execa } from "execa";
+import enquirer from "enquirer";
+import { waitForEnter } from "../utils.js";
+
+export default async function publishToNpm({ dry }) {
+  console.log(`Ready to publish to NPM${dry ? "(--dry-run)" : ""}`);
+
+  await waitForEnter();
+
+  const commonArgs = ["publish"];
+  if (dry) {
+    commonArgs.push("--dry-run");
+  }
+
+  const runNpmPublish = async () => {
+    const args = [...commonArgs];
+
+    if (!dry) {
+      const { otp } = await enquirer.prompt({
+        type: "input",
+        name: "otp",
+        message: "Please enter your npm OTP",
+      });
+      args.push("--otp", otp);
+    }
+
+    await execa("npm", args, { cwd: "./dist" });
+  };
+
+  /**
+   * Retry "npm publish" when to enter OTP is failed.
+   */
+  for (let i = 5; i > 0; i--) {
+    try {
+      return await runNpmPublish();
+    } catch (error) {
+      if (error.code === "EOTP" && i > 0) {
+        console.log(`To enter OTP is failed, you can retry it ${i} times.`);
+        continue;
+      }
+      throw error;
+    }
+  }
+}
---
+++ scripts/release/steps/update-changelog.js
@@ -0,0 +1,76 @@
+import fs from "node:fs";
+import { execa } from "execa";
+import chalk from "chalk";
+import semver from "semver";
+import {
+  waitForEnter,
+  runYarn,
+  logPromise,
+  getBlogPostInfo,
+  getChangelogContent,
+} from "../utils.js";
+
+function writeChangelog(params) {
+  const changelog = fs.readFileSync("CHANGELOG.md", "utf8");
+  const newEntry = `# ${params.version}\n\n` + getChangelogContent(params);
+  fs.writeFileSync("CHANGELOG.md", newEntry + "\n\n" + changelog);
+}
+
+async function getChangelogForPatch({ version, previousVersion }) {
+  const { stdout: changelog } = await execa("node", [
+    "scripts/changelog-for-patch.js",
+    "--prev-version",
+    previousVersion,
+    "--new-version",
+    version,
+  ]);
+  return changelog;
+}
+
+export default async function updateChangelog({
+  dry,
+  version,
+  previousVersion,
+}) {
+  if (dry) {
+    return;
+  }
+
+  const semverDiff = semver.diff(version, previousVersion);
+
+  if (semverDiff !== "patch") {
+    const blogPost = getBlogPostInfo(version);
+    writeChangelog({
+      version,
+      previousVersion,
+      body: `🔗 [Release Notes](https://prettier.io/${blogPost.path})`,
+    });
+    if (fs.existsSync(blogPost.file)) {
+      // Everything is fine, this step is finished
+      return;
+    }
+    console.warn(
+      `${chalk.yellow("warning")} The file ${chalk.bold(
+        blogPost.file,
+      )} doesn't exist, but it will be referenced in ${chalk.bold(
+        "CHANGELOG.md",
+      )}. Make sure to create it later.`,
+    );
+  } else {
+    const body = await getChangelogForPatch({
+      version,
+      previousVersion,
+    });
+    writeChangelog({
+      version,
+      previousVersion,
+      body,
+    });
+  }
+
+  await waitForEnter();
+  await logPromise(
+    "Re-running Prettier on docs",
+    runYarn(["lint:prettier", "--write"]),
+  );
+}
